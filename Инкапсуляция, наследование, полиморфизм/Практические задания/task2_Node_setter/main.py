77b0a80a52afa6fa681b108c438c5123f151877f
alemen <t@.ru>
2022-01-13 20:48:25 +0300

    13.01.22

---
+++ Инкапсуляция, наследование, полиморфизм/Практические задания/task2_Node_setter/main.py
@@ -0,0 +1,48 @@
+from typing import Any, Optional
+
+
+class Node:
+    """ Класс, который описывает узел связного списка. """
+
+    def __init__(self, value: Any, next_: Optional["Node"] = None):
+        """
+        Создаем новый узел для односвязного списка
+        :param value: Любое значение, которое помещено в узел
+        :param next_: следующий узел, если он есть
+        """
+        self.value = value
+
+        # TODO заменить на вызов setter
+        self.next = next_
+        # self.set_next(next_)
+
+    def __repr__(self) -> str:
+        return f"Node({self.value}, {None})" if self.next is None else f"Node({self.value}, Node({self.next}))"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def is_valid(self, node: Any) -> None:
+        if not isinstance(node, (type(None), Node)):
+            raise TypeError
+
+    @property
+    def next(self):
+        return self._next
+
+
+
+    @next.setter
+    def next(self, next_: Optional["Node"] = None) -> None:
+        print('Вызван setter')
+        self.is_valid(next_)
+        self._next = next_
+
+
+if __name__ == "__main__":
+    first_node = Node(1)  # отработал setter в init
+    second_node = Node(2)  # отработал setter в init
+
+    first_node.next = second_node
+
+    print(repr(first_node), repr(first_node.next))
---
+++ Инкапсуляция, наследование, полиморфизм/Практические задания/task4_Figure_polymorphism_inheritance/main.py
@@ -0,0 +1,46 @@
+import math
+
+
+class Figure:
+    """ Базовый класс. """
+
+    def area(self):
+        print(f"Вызван метод класса {self.__class__.__name__}")
+        return None
+
+
+class Rectangle(Figure):
+    """ Производный класс. Прямоугольник. """
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def area(self):
+        print(f"Вызван метод класса {self.__class__.__name__}")
+        return self.a * self.b
+
+
+class Circle(Figure):
+    """ Производный класс. Круг. """
+    PI = 3.14
+
+    def __init__(self, r):
+        self.r = r
+        # self.PI = 3.14
+
+    def area(self):
+        print(f"Вызван метод класса {self.__class__.__name__}")
+        return self.r * self.PI
+
+
+
+if __name__ == "__main__":
+    fig = Figure()
+    fig.area()
+
+    rect = Rectangle(5, 10)
+    rect.area()
+
+    circle = Circle(5)
+    circle.area()
